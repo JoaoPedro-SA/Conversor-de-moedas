14a89cf8bcfc170816a85f3fc71e209e8c1b1d85
Alan <alanaraujosilveira6@gmail.com>
2025-01-18 16:31:55 -0300

    Update Main.java
    Agora a aplicação faz conversão com qualquer moeda, não mais limitada a 6 tipos de conversões

--- Main.java
+++ Main.java
@@ -5,47 +5,27 @@ import java.util.Scanner;
         public static void main(String[] args) {
             Scanner leitura = new Scanner(System.in);
 
-            System.out.println("Digite uma das opções a seguir:");
-            System.out.println("""
-                    1) Real para Dólar;
-                    2) Real para Euro;
-                    3) Real para peso argentino;
-                    4) Dólar para Real;
-                    5) Dólar para Euro;
-                    6) Dólar para peso argentino.
-                           
-                    """);
-            var opcao = leitura.nextInt();
-            System.out.println("Insira a quantia para verificar a cotação:");
-            var valor = leitura.nextDouble();
+            System.out.println("Digite uma moeda para verificar a cotação: ");
+            System.out.println("Exemplos de moedas aceitas: BRL, USD, EUR ");
 
-            String moeda1 = "";
-            String moeda2 = "";
+            var moeda1 = leitura.nextLine();
+            moeda1 = moeda1.toUpperCase();
+            if (moeda1.length() > 3){
+                System.out.println("A moeda: "+ moeda1 +" é invalida" );
+            } else{
+                System.out.println("Digite moeda para a comparação: ");
+                var moeda2 = leitura.nextLine();
+                moeda2 = moeda2.toUpperCase();
 
-            if (opcao < 4 && opcao > 0) {
-                moeda1 = "BRL";
-            } else if (opcao < 6) {
-                moeda1 = "USD";
-            } else {
-                System.out.println("Seleção incorreta");
-            }
+                if (moeda2.length() > 3){
+                    System.out.println("A moeda: "+ moeda2 +" é invalida" );
+                } else{
 
-            if(opcao == 2 || opcao == 5){
-                moeda2 = "EUR";
-            }
-            if(opcao == 3 || opcao == 6){
-                moeda2 = "ARS";
-            }
-            if(opcao == 1){
-                moeda2 = "USD";
-            }
-            if(opcao == 4){
-                moeda2 = "BRL";
-            }
+                    System.out.println("Insira a quantia para verificar a cotação:");
+                    var valor = leitura.nextDouble();
 
-                Moeda novaMoeda = Conversor.buscaMoeda(moeda1, moeda2, valor);
+                    Moeda novaMoeda = Conversor.buscaMoeda(moeda1);
 
-
-                System.out.println(moeda1 +" "+ valor + " equivalem á: "+ moeda2 +" " + novaMoeda.conversion_rates().get(moeda2) * valor);
-        }}
+                    System.out.println(moeda1 +" "+ valor + " equivalem á: "+ moeda2 +" " + novaMoeda.conversion_rates().get(moeda2) * valor);
+        }}}}
 
