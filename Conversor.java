3325c4dc38467c9f7a3502caabc41193b59684a4
Alan <alanaraujosilveira6@gmail.com>
2025-01-18 16:32:52 -0300

    Update Conversor.java
    Dados redundantes na execução da classe foram removidos

--- Conversor.java
+++ Conversor.java
@@ -6,7 +6,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Conversor {
-        public static Moeda buscaMoeda(String moedaBase, String moedaConvertida, double quantia) {
+        public static Moeda buscaMoeda(String moedaBase) {
             URI conversor = URI.create("https://v6.exchangerate-api.com/v6/faa274f4e80f30d8b025fc85/latest/"+ moedaBase);
 
             HttpRequest request = HttpRequest.newBuilder()
@@ -23,9 +23,5 @@ public class Conversor {
             } catch (Exception e) {
                 throw new RuntimeException("Não consegui obter o valor da moeda");
             }
-
         }
-
-
 }
-
